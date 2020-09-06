426f78265c46de5794a4c227cc08855ab6faac9d
buchi-1996 <ukaigweonyebuchi@gmail.com>
2020-09-06 04:06:00 +0100

    new

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
+ const staticCacheName = 'static-site';
+ const assets = [
+     '/',
+     '/index.html',
+     '/js/index.js',
+     '/js/app.js',
+     '/css/style.css',
+     '/img/bora_bora_bungalows-wallpaper-1024x768.jpg',
+     '/img/energy.png',
+     '/img/Ripple-1s-200px.gif',
+     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+     'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap',
+ ]
  self.addEventListener('install', e => {
-     console.log('service worker has been installed');
+    //  console.log('service worker has been installed');
+    e.waitUntill(caches.open(staticCacheName)
+    .then(cache => {
+        console.log('caching assets');
+        cache.addAll(assets);
+    }))
+    
  })
 
  self.addEventListener('activate', e => {
@@ -7,5 +26,12 @@
  })
 
  self.addEventListener('fetch', e => {
-     console.log('fetching data', e);
+    //  console.log('fetching data', e);
+
+    e.respondWith(
+        caches.match(e.request)
+        .then(cacheRes => {
+            return cacheRes || fetch(e.request);
+        })
+    )
  })
