ecf5b4a1dc218691f4f8f02602d4e5870ba38be9
buchi-1996 <ukaigweonyebuchi@gmail.com>
2020-09-10 15:08:51 +0100

    new commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,23 @@
 
+class Weather{
+    constructor(city){
+        this.city = city;
+        this.apiKey = 'aaf6e61337fd5555767d7bd243858055';
+        this.loader = document.querySelector('#img__loader');
+    }
+
+    async getWeather(){
+        const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`)
+        const resData = await response.json();
+        this.loader.style.display = 'none';
+        return resData;
+    }
+
+    changeCity(city){
+        this.city = city;
+    }
+}
+
 
 class App{
     constructor(){
@@ -94,33 +113,13 @@ class Ui{
         const myCity = new Cities();
          myCity.getCities()
          .then(data => {
-            const list = data.map(x => `<p class="list__item"><img src=${x.flag} class="flag__img">${x.city}</p>`).join('');
+            const list = data.map(x => `<p class="list__item"><img src=${x.flag} alt="flag" class="flag__img">${x.city}</p>`).join('');
             this.cities.innerHTML = list;
          });
     }
 
 }
 
-
-
-class Weather{
-    constructor(city){
-        this.city = city;
-        this.apiKey = 'aaf6e61337fd5555767d7bd243858055';
-        this.loader = document.querySelector('#img__loader');
-    }
-
-    async getWeather(){
-        const response =  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`)
-        const resData = await response.json();
-        this.loader.style.display = 'none';
-        return resData;
-    }
-
-    changeCity(city){
-        this.city = city;
-    }
-}
 
 
 class Cities {
@@ -156,8 +155,6 @@ class DateApp{
     }
 }
 
-
-
 class Storage{
     constructor(){
         this.city;
@@ -182,12 +179,13 @@ class Storage{
 }
 
 
-
-
-
 const myStore = new Storage();
 const getCity = myStore.getData();
 const myWeather = new Weather(getCity.city);
+
+
+
+
 // =============================================================
             // EVENT LISTENERS
 //============================================================
--- public/sw.js
+++ public/sw.js
@@ -5,18 +5,17 @@
      '/js/index.js',
      '/js/app.js',
      '/css/style.css',
-     '/img/bora_bora_bungalows-wallpaper-1024x768.jpg',
+     '/img/bora_bora_bungalows-wallpaper-1024x768 (1).webp',
      '/img/energy.png',
      '/img/Ripple-1s-200px.gif',
-     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
      'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap',
      
      
  ]
- self.addEventListener('install', e => {
+ self.addEventListener('install', evt => {
     //  console.log('service worker has been installed');
-    // self.skipWaiting();
-    e.waitUntill(caches.open(staticCacheName)
+    self.skipWaiting();
+    evt.waitUntill(caches.open(staticCacheName)
     .then(cache => {
         console.log('caching assets');
         cache.addAll(assets);
